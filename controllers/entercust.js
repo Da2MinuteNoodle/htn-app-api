daede18d33d8d8b64d48a806183fe9f24a5aa06a
Maxs <40285083+MaxsR6@users.noreply.github.com>
2018-07-22 01:24:19 +1000

    Finishing entercust

---
+++ controllers/entercust.js
@@ -0,0 +1,21 @@
+const handleEnterCust = (req, res, db) => {
+  const { name, phone, dogname, dogbreed, dogsize, dogage, specialneeds, groomedbefore } = req.body;
+  if (!name || !phone || !dogname || !dogbreed || !dogsize || !dogage) {
+    return res.status(400).json('Incorrect form submission');
+  }
+  db('customers').insert({
+    name: name,
+    phone: phone,
+    dogname: dogname,
+    dogbreed: dogbreed,
+    dogsize: dogsize,
+    dogage: dogage,
+    specialneeds: specialneeds,
+    groomedbefore: groomedbefore
+  })
+  .catch(err => res.status(400).json('unable to enter customer'))
+}
+
+module.exports = {
+  handleEnterCust
+};
