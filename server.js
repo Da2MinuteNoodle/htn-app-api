5ddb6f45bbfff4eefe3c96f00c4f88c5279c610e
Maxs <40285083+MaxsR6@users.noreply.github.com>
2018-07-21 22:00:28 +1000

    Adding Session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
+const session = require('express-session');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -21,6 +22,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(session({secret:"test",resave:false,saveUninitialized:true}))
 
 app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
