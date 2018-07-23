e2c7cdab7448a379dc3cfdf393d3721332cc07ed
Maxs <40285083+MaxsR6@users.noreply.github.com>
2018-07-23 20:40:07 +1000

    Fixing json

--- controllers/viewcust.js
+++ controllers/viewcust.js
@@ -1,6 +1,6 @@
 const handleViewCust = (req, res, db) => {
-  db.select().from('customers').then(function(data) {
-    res.send(data)
+  db.select().from('customers').then(data => {
+    res.json(data)
   })
   .catch(err => res.status(400).json('unable to fetch data customer'))
 }
