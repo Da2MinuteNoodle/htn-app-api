b82d0b563b1e46f14b3143f5276fe8b5afd0de74
Maxs <40285083+MaxsR6@users.noreply.github.com>
2018-07-29 22:47:39 +1000

    Fix

--- controllers/editcust.js
+++ controllers/editcust.js
@@ -1,5 +1,5 @@
 const handleEditCust = (req, res, db) => {
-  const { id, name, phone, dogname, dogbreed, dogsize, dogage, specialneeds, groomedbefore } = req.body;
+  const { id, name, phone, dogname, dogbreed, dogsize, dogage, specialneeds, groomedbefore, next_appt } = req.body;
   if (!id) {
     return res.status(400).json('Incorrect form submission');
   }
@@ -11,7 +11,8 @@ const handleEditCust = (req, res, db) => {
     dogsize: dogsize,
     dogage: dogage,
     specialneeds: specialneeds,
-    groomedbefore: groomedbefore
+    groomedbefore: groomedbefore,
+    next_appt: next_appt,
   }).then(res.status(200).json('Customer Changed'))
   .catch(err => res.status(400).json('unable to enter customer'))
 }
