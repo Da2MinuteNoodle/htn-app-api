4c6f1d957b39e5beef155706bbf65be2021096ea
Maxs <40285083+MaxsR6@users.noreply.github.com>
2018-08-05 20:48:29 +1000

    Fix For Next Appt

--- controllers/editcust.js
+++ controllers/editcust.js
@@ -3,17 +3,30 @@ const handleEditCust = (req, res, db) => {
   if (!id) {
     return res.status(400).json('Incorrect form submission');
   }
-  db('customers').where({ id: id }).update({
-    name: name,
-    phone: phone,
-    dogname: dogname,
-    dogbreed: dogbreed,
-    dogsize: dogsize,
-    dogage: dogage,
-    specialneeds: specialneeds,
-    groomedbefore: groomedbefore,
-    next_appt: next_appt,
+  if (next_appt === null) {
+    db('customers').where({ id: id }).update({
+      name: name,
+      phone: phone,
+      dogname: dogname,
+      dogbreed: dogbreed,
+      dogsize: dogsize,
+      dogage: dogage,
+      specialneeds: specialneeds,
+      groomedbefore: groomedbefore,
+    }).then(res.status(200).json('Customer Changed'))
+  } else {
+    db('customers').where({ id: id }).update({
+      name: name,
+      phone: phone,
+      dogname: dogname,
+      dogbreed: dogbreed,
+      dogsize: dogsize,
+      dogage: dogage,
+      specialneeds: specialneeds,
+      groomedbefore: groomedbefore,
+      next_appt: next_appt,
   }).then(res.status(200).json('Customer Changed'))
+}
   .catch(err => res.status(400).json('unable to enter customer'))
 }
 
